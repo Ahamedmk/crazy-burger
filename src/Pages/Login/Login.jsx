f86b8555b7523ddd7645a6c6a15d03bac2271d83
Ahamedmk <amkavavo1@gmail.com>
2023-08-04 11:55:02 +0200

    mon premier commit

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+
+ export function Login() {
+
+    const [name, setName] = useState("")
+
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        alert (`Bonjour ${name}` )
+        setName("")
+    }
+    return (
+        <>
+          <h1>Bienvenue chez nous !</h1>
+          <h2>Connectez vous</h2>
+          <form onSubmit={handleClick} >
+          <input type="text" value={name} onChange={e => setName(e.target.value)} required="required" placeholder="Entrez votre prénom..." />
+          <button >Accédez à votre espace</button>
+          </form>
+          
+        </>
+      )
+
+}
