ebe7faa98873b38bf6b15139ff38d8be7568bcbd
Ahamedmk <amkavavo1@gmail.com>
2023-08-12 22:23:56 +0200

    💄 F03: add background image

--- src/Pages/Login/LoginForm.jsx
+++ src/Pages/Login/LoginForm.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react"
 import {useNavigate } from "react-router-dom"
+import burger from "../../assets/burger-background.jpg"
 import {styled} from "styled-components"
 import { theme } from "../../theme"
+import{BsPersonCircle} from "react-icons/bs"
 
 
  export function LoginForm() {
@@ -18,10 +20,17 @@ import { theme } from "../../theme"
     return (
         <>
           <LoginFormStyled onSubmit={handleClick} >
-          <h1>Bienvenue chez nous !</h1>
-          <h2>Connectez vous</h2>
-          <input type="text" value={name} onChange={e => setName(e.target.value)} required="required" placeholder="Entrez votre prénom..." />
-          <button >Accédez à votre espace</button>
+          <div className="first-container">
+            <div className="container">
+              <h1>Bienvenue chez nous !</h1>
+              <h2>Connectez vous</h2>
+              <div className="container-form">
+              <BsPersonCircle className="icon" />
+                <input type="text" value={name} onChange={e => setName(e.target.value)} required="required" placeholder="Entrez votre prénom "/>
+                <button >Accéder à mon espace</button>
+              </div>
+            </div>
+          </div>
           </LoginFormStyled>  
         </>
       )
@@ -29,9 +38,49 @@ import { theme } from "../../theme"
 }
  const LoginFormStyled = styled.form`
  border: 1px solid blue ;
+ background-image: url('../../assets/burger-background.jpg');
+  background-size: cover;
+  background-position: center;
+ height: 100vh;
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ 
+ .first-container {
+  border: 3px solid yellowgreen ;
+  background-image: url(${burger});
+  background-size: cover;
+  background-position: center;
+ height: 100vh;
+ width: 100%;
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ }
+ 
+ .container {
+  position: relative;
+  display: inline-block;
+  border: 1px solid green;
+ }
+ .container-form {
+  display: flex;
+  flex-direction: column;
+  border: 1px solid red;
+ }
+  .icon {
+  position: absolute;
+  /* top: 50%; */
+  /* transform: translateY(-70%); */
+  left: 8px;
+}
+ 
  h1 {
    font-size:50px;
    color:${theme.colors.greyBlue};
    background-color:green;
+ }
+ h2 {
+  text-align: center;
  }
  `
