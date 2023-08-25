7dbe2f90f61f1b34b14d6b405118ed5c16667481
Ahamedmk <amkavavo1@gmail.com>
2023-08-25 22:49:25 +0200

    ✨ F05: create menu burger

--- src/Pages/OrderPage/Main/Main.jsx
+++ src/Pages/OrderPage/Main/Main.jsx
@@ -1,8 +1,18 @@
 import { styled } from "styled-components";
 import { theme } from "../../../theme";
+import { fakeMenu1 } from "../../../fakeData/fakeMenu";
 
 export default function Main() {
-  return <MainStyles>Main</MainStyles>;
+  return <MainStyles>
+    <div className="CardMenu">
+      <div className="Burger">
+      <img src={fakeMenu1[0].imageSource} alt="burger" />
+      </div>
+      <div>
+        <h1>{fakeMenu1[0].title}</h1>
+      </div>
+    </div>
+  </MainStyles>;
 }
 const MainStyles = styled.div`
   height: 83vh;
@@ -11,4 +21,23 @@ const MainStyles = styled.div`
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   border-radius: 0px 0px 15px 15px;
   margin: 0 auto;
+  .CardMenu {
+    width: 240px;
+    height:330px;
+    border: 1px solid red ;
+  }
+  .Burger {
+    display: flex;
+    justify-content: center;
+    border: 1px solid blue ;
+    margin-top: 50px;
+  
+  img {
+    width:200px;
+    height:145px;
+    border: 3px solid greenyellow ;
+    
+
+  }
+}
 `;
---
+++ src/Pages/OrderPage/Main/Menu.jsx
@@ -0,0 +1,12 @@
+
+import { styled } from 'styled-components';
+
+export default function Menu() {
+  return (
+    <MenuSyled>Menu</MenuSyled>
+  )
+}
+ 
+const MenuSyled = styled.div`
+  
+`;
